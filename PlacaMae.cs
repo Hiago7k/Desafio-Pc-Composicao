39b842abfb799dace8a59273da5cb2b6b25937c6
Hiago 7k <hiago7k@gmail.com>
2026-03-04 21:07:18 -0300

    criando classe placa mae

---
+++ PlacaMae.cs
@@ -0,0 +1,13 @@
+﻿namespace Desafio_Pc_Composicao;
+
+internal class PlacaMae
+{
+    public PlacaMae(string fabricante, string socket)
+    {
+        Fabricante = fabricante;
+        Socket = socket;
+    }
+
+    public string Fabricante { get;  }
+    public string Socket { get;  }
+}
