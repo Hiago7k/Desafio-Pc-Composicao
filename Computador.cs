1143b7b331a7b99738c90b241876dc760ca3c154
Hiago 7k <hiago7k@gmail.com>
2026-03-04 21:08:56 -0300

    criando classe computador usando composicao para pegar propriedades das outras classes

---
+++ Computador.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Pc_Composicao
+{
+    internal class Computador
+    {
+    }
+}
